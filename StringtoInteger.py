59790306df3f5e70c0d83033f797ee37382ed921
doitfool <doitfoolac@gmail.com>
2016-03-26 23:06:41 +0800

    modify StringtoInteger

--- StringtoInteger.py
+++ StringtoInteger.py
@@ -2,14 +2,16 @@
 """
 Implement atoi to convert a string to an integer.
 整形范围：INT_MAX (2147483647) or INT_MIN (-2147483648)
+leetcode python编译器的sys.maxint不是2147483647
 """
-import sys
+
 class Solution(object):
     def myAtoi(self, str):
         """
         :type str: str
         :rtype: int
         """
+        maxint = 2147483647
         if str == None or len(str) < 1:
             return 0
         temp_str = str.strip()
@@ -24,19 +26,16 @@ class Solution(object):
         result = 0
         while i < len(temp_str) and temp_str[i] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
             result = result*10 + int(temp_str[i])
-            if result > sys.maxint:
+            if result > maxint:
                 break
             i += 1
         if symbol:
-            if result > sys.maxint:
-                result = sys.maxint
+            if result > maxint:
+                result = maxint
         else:
-            if result > sys.maxint+1:
-                result = -(sys.maxint+1)
+            if result > maxint+1:
+                result = -(maxint+1)
             else:
                 result = -result
         return result
 
-if __name__ == '__main__':
-    s = Solution()
-    print s.myAtoi('     +2147483648 ')
