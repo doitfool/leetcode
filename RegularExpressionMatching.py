2f9fa920eddcfdc2f60eec385b37d9e29f1fb1e7
doitfool <doitfoolac@gmail.com>
2016-04-05 23:31:33 +0800

    add  RegularExpressionMatching

--- RegularExpressionMatching.py
+++ RegularExpressionMatching.py
@@ -6,45 +6,61 @@
 	@Description:   Implement regular expression matching with support for '.' and '*'.
 """
 
+
 class Solution(object):
-    def convert(self, s):
-        t = []
-        i = 0
-        while i < len(s):
-            c = s[i]
-            count = 1
-            i += 1
-            while i < len(s) and s[i]==c:
-                count += 1
-                i += 1
-            if i < len(s) and s[i]=='*':
-                count = -1
-                i += 1
-            t.append((c, count))
-        return t
-
-
     def isMatch(self, s, p):
         """
         :type s: str
         :type p: str
         :rtype: bool
         """
-        ss = self.convert(s)
-        pp = self.convert(p)
-        match = True
-        for i in xrange(min(len(ss), len(pp))):
-            print ss[i][0], ss[i][1], pp[i][0], pp[i][1]
-            if ss[i][0] != pp[i][0]:
-                if pp[i][0] != '.':
-                    match = False
-                    break
-            elif ss[i][1] != pp[i][1]:
-                if pp[i][1] != -1:
-                    match = False
-                    break
-        return match
+        m = len(s)
+        n = len(p)
+        # Init dp
+        dp = [[False for i in range(n + 1)] for i in range(m + 1)]
+        # When string and pattern are all None
+        dp[m][n] = True
+        # When the string is None, pattern like "a*" can still match it
+        for i in range(n - 1, -1, -1):
+            if p[i] == "*":
+                dp[m][i] = dp[m][i + 1]
+            elif i + 1 < n and p[i + 1] == "*":
+                dp[m][i] = dp[m][i + 1]
+            else:
+                dp[m][i] = False
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                # When the current character is "*"
+                if p[j] == "*":
+                    if j - 1 >= 0 and p[j - 1] != "*":
+                        dp[i][j] = dp[i][j + 1]
+                    # If the pattern is starting with "*" or has "**" in it
+                    else:
+                        return False
+                # When the the second character of pattern is "*"
+                elif j + 1 < n and p[j + 1] == "*":
+                    # When the current character matches, there are three possible situation
+                    # 1. ".*" matches nothing
+                    # 2. "c*" matches more than one character
+                    # 3. "c*" just matches one character
+                    if s[i] == p[j] or p[j] == ".":
+                        dp[i][j] = dp[i][j + 2] or dp[i + 1][j] or dp[i + 1][j + 2]
+                    # Ignore the first two characters("c*") in pattern since they cannot match
+                    # the current character in string
+                    else:
+                        dp[i][j] = dp[i][j + 2]
+                else:
+                    # When the current character is matched
+                    if s[i] == p[j] or p[j] == ".":
+                        dp[i][j] = dp[i + 1][j + 1]
+                    else:
+                        dp[i][j] = False
+        return dp[0][0]
 
 if __name__ == '__main__':
     s = Solution()
-    print s.isMatch('aab', 'c*a*b*')
+    x = ['aa', 'aa', 'aaa', 'aa', 'aa', 'ab', 'aab', 'abcd', 'aaa', 'aa', 'abcd']
+    y = ['a', 'aa', 'aa', 'a*', '.*', '.*', 'c*a*b', 'd*', 'a*a', 'a*.*', '.*']
+    for i in xrange(len(x)):
+        print s.isMatch(x[i], y[i])
