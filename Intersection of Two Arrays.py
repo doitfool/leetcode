fb979785f6456125817b9fe24f790260881f8e8b
doitfool <doitfoolac@gmail.com>
2016-06-04 23:29:01 +0800

    intersection of two array

---
+++ Intersection of Two Arrays.py
@@ -0,0 +1,38 @@
+"""
+    @Project: leetcode
+    @file: Intersection of Two Arrays.py
+    @author: AC
+    @time: 16-6-4
+    @Description:
+    Given two arrays, write a function to compute their intersection.
+
+    Example:
+    Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+    Note:
+    Each element in the result must be unique.
+    The result can be in any order.
+"""
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        result = set()
+        i, j = 0, 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                result.add(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                i += 1
+
+        return list(result)
